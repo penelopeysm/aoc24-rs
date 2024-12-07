01a89264d6e94f85e060da84240888501b3dc7bf
Penelope Yong <penelopeysm@gmail.com>
2024-12-07 15:03:00 +0000

    Document + some optimisations

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -9,7 +9,7 @@ use std::collections::HashMap;
 
 struct Countdown {
     target: u64,
-    numbers: Vec<u64>,            // TODO use NonEmpty
+    numbers: Vec<u64>,
     cache: HashMap<Vec<Op>, u64>, // Mapping of operators -> results
 }
 
@@ -21,50 +21,75 @@ enum Op {
 }
 
 impl Countdown {
-    fn calculate(&mut self, ops: &[Op]) -> u64 {
+    /// For a series of operators ops[0], ops[1], ..., ops[i], calculate
+    /// the value of the expression (self.numbers[0] ops[0] self.numbers[1]
+    /// ops[1] ... ops[i] self.numbers[i+1]), with left associativity.
+    ///
+    /// Note that the list of operators may be shorter than the list of numbers
+    /// (minus 1), in which case the numbers are not fully used.
+    ///
+    /// Returns None if the result is known to be larger than self.target.
+    /// Otherwise returns Some(result).
+    fn calculate(&mut self, ops: &[Op]) -> Option<u64> {
         // Base case: if the ops are empty, just return the first number
         if ops.is_empty() {
             self.cache.insert(ops.to_vec(), self.numbers[0]);
-            return self.numbers[0];
+            return Some(self.numbers[0]);
         }
 
         // If it's already been cached, return it
-        match self.cache.get(ops) {
-            Some(v) => *v,
+        if let Some(v) = self.cache.get(ops) {
+            return Some(*v);
+        }
 
-            // Otherwise, recurse
-            None => {
-                let acc = self.calculate(&ops[..ops.len() - 1]);
+        // Otherwise, recurse
 
-                // Notice that the operations involved only lead to the accumulator
-                // getting larger. So, if our acc is already larger than the target,
-                // we can just shortcircuit and return the largest value. This cuts
-                // about 20% off the time.
-                if acc >= self.target {
-                    return u64::MAX;
-                }
+        // Notice that the operations involved only lead to the accumulator
+        // getting larger. So, if our accumulator is already larger than the
+        // target, we can just shortcircuit and return None. This cuts about
+        // 20-30% off the time.
+        let acc = self.calculate(&ops[..ops.len() - 1]);
+        match acc {
+            // The previous calculation was already larger than the target
+            None => None,
 
+            Some(a) => {
                 let next = self.numbers[ops.len()];
                 let result = match ops[ops.len() - 1] {
-                    Op::Add => acc + next,
-                    Op::Mul => acc * next,
+                    Op::Add => a + next,
+                    Op::Mul => a * next,
                     Op::Concat => {
                         let n_digits_next = next.to_string().len();
-                        acc * 10u64.pow(n_digits_next as u32) + next
+                        a * 10u64.pow(n_digits_next as u32) + next
                     }
                 };
-                self.cache.insert(ops.to_vec(), result);
-                result
+                if result > self.target {
+                    None
+                }
+                else {
+                    self.cache.insert(ops.to_vec(), result);
+                    Some(result)
+                }
             }
         }
     }
 
-    fn solve(&mut self, allowed_ops: Vec<Op>) -> Option<(Vec<Op>, u64)> {
+    /// Given a list of allowed operators allowed_ops, find a combination
+    /// of operators ops for which self.calculate(ops) == self.target and every
+    /// element of ops is in allowed_ops.
+    ///
+    /// Note: The allowed operations must always lead to a result that is larger
+    /// than both of its operands. Thus, if an operator like Minus is passed,
+    /// this function may not work correctly.
+    /// 
+    /// If there is no solution, returns None. Otherwise, returns Some((ops, target)),
+    /// where ops is the list of operators that lead to the target.
+    fn solve(mut self, allowed_ops: Vec<Op>) -> Option<(Vec<Op>, u64)> {
         let n = self.numbers.len() - 1;
         let allowed_ops_combinations = (0..n).map(|_| allowed_ops.clone().into_iter());
         allowed_ops_combinations
             .multi_cartesian_product()
-            .find(|ops| self.calculate(&ops[..]) == self.target)
+            .find(|ops| self.calculate(&ops[..]) == Some(self.target))
             .map(|maybe_sol| (maybe_sol, self.target))
     }
 }
