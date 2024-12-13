f1c3339a1490e92d8a8073917da109696b825f08
Penelope Yong <penelopeysm@gmail.com>
2024-12-13 23:53:48 +0000

    Refactor Day 10 to use generics

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -4,20 +4,24 @@ use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct Point {
+struct Point<T: IntoIterator<Item = (u32, u32)> + FromIterator<(u32, u32)>> {
     row: u32,
     col: u32,
-    reachable_nines: BTreeSet<(u32, u32)>,
-    score: u32,
+    reachable_nines: T,
 }
 
-fn is_adjacent(p1: &Point, p2: &Point) -> bool {
+fn is_adjacent<T: IntoIterator<Item = (u32, u32)> + FromIterator<(u32, u32)>>(
+    p1: &Point<T>,
+    p2: &Point<T>,
+) -> bool {
     p1.row.abs_diff(p2.row) == 1 && p1.col.abs_diff(p2.col) == 0
         || (p1.row.abs_diff(p2.row) == 0 && p1.col.abs_diff(p2.col) == 1)
 }
 
-fn sum_trailheads(mut map: BTreeMap<u32, Vec<Point>>) -> (u32, u32) {
-    // For each value of N < 9, sum the scores of all adjacent N+1's
+fn sum_trailheads<T: Clone + IntoIterator<Item = (u32, u32)> + FromIterator<(u32, u32)>>(
+    mut map: BTreeMap<u32, Vec<Point<T>>>,
+) -> u32 {
+    // For each value of N < 9, collect all the reachable nines of its neighbours that are N+1
     for n in (0..9).rev() {
         let np1_points = map.remove(&(n + 1)).unwrap();
         let n_points = map.get_mut(&n).unwrap();
@@ -27,26 +31,18 @@ fn sum_trailheads(mut map: BTreeMap<u32, Vec<Point>>) -> (u32, u32) {
                 .filter(|p| is_adjacent(point, p))
                 .flat_map(|p| p.reachable_nines.clone())
                 .collect();
-            point.score = np1_points
-                .iter()
-                .filter(|p| is_adjacent(point, p))
-                .fold(0, |acc, p| acc + p.score);
         }
     }
-    // Return part 1 and part 2 together
-    let p1_sol = map.get(&0)
-        .unwrap()
-        .iter()
-        .fold(0, |acc, p| acc + p.reachable_nines.len() as u32);
-    let p2_sol = map.get(&0)
-        .unwrap()
-        .iter()
-        .fold(0, |acc, p| acc + p.score);
-    (p1_sol, p2_sol)
+    map.remove(&0).unwrap().into_iter().fold(0, |acc, p| {
+        // .len() isn't provided by a trait, so .into_iter().count() is used instead
+        acc + p.reachable_nines.into_iter().count() as u32
+    })
 }
 
-fn parse_input(input: &str) -> BTreeMap<u32, Vec<Point>> {
-    let mut map: BTreeMap<u32, Vec<Point>> = BTreeMap::new();
+fn parse_input<T: Default + IntoIterator<Item = (u32, u32)> + FromIterator<(u32, u32)>>(
+    input: &str,
+) -> BTreeMap<u32, Vec<Point<T>>> {
+    let mut map: BTreeMap<u32, Vec<Point<T>>> = BTreeMap::new();
     for i in 0..10 {
         map.insert(i, Vec::new());
     }
@@ -55,15 +51,15 @@ fn parse_input(input: &str) -> BTreeMap<u32, Vec<Point>> {
             match char.to_digit(10) {
                 None => panic!("Invalid input"),
                 Some(n) => {
-                    let mut point = Point {
+                    let point = Point {
                         row: i as u32,
                         col: j as u32,
-                        reachable_nines: BTreeSet::new(),
-                        score: if n == 9 { 1 } else { 0 },
+                        reachable_nines: if n == 9 {
+                            [(i as u32, j as u32)].into_iter().collect()
+                        } else {
+                            T::default()
+                        },
                     };
-                    if n == 9 {
-                        point.reachable_nines.insert((i as u32, j as u32));
-                    }
                     map.get_mut(&n).unwrap().push(point);
                 }
             }
@@ -73,11 +69,11 @@ fn parse_input(input: &str) -> BTreeMap<u32, Vec<Point>> {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(sum_trailheads(parse_input(input)).0)
+    Some(sum_trailheads(parse_input::<BTreeSet<_>>(input)))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    Some(sum_trailheads(parse_input(input)).1)
+    Some(sum_trailheads(parse_input::<Vec<_>>(input)))
 }
 
 #[cfg(test)]
