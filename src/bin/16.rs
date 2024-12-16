9685a0725dec6862ffcbd0fe87d88bc424e04598
Penelope Yong <penelopeysm@gmail.com>
2024-12-16 13:55:54 +0000

    Optimise graph construction

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -10,138 +10,207 @@ enum Dir {
     E,
     W,
 }
+impl Dir {
+    fn clockwise(&self) -> Self {
+        match self {
+            Dir::N => Dir::E,
+            Dir::E => Dir::S,
+            Dir::S => Dir::W,
+            Dir::W => Dir::N,
+        }
+    }
+    fn anticlockwise(&self) -> Self {
+        match self {
+            Dir::N => Dir::W,
+            Dir::W => Dir::S,
+            Dir::S => Dir::E,
+            Dir::E => Dir::N,
+        }
+    }
+}
 
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct Node {
+// // Get the cost of travelling from n1 to n2
+// fn _get_edge_weight(n1: &Node, n2: &Node) -> Option<u32> {
+//     // Rotating
+//     if n1.i == n2.i && n1.j == n2.j {
+//         match (n1.dir, n2.dir) {
+//             (Dir::N, Dir::E) => Some(1000),
+//             (Dir::N, Dir::W) => Some(1000),
+//             (Dir::E, Dir::N) => Some(1000),
+//             (Dir::E, Dir::S) => Some(1000),
+//             (Dir::S, Dir::E) => Some(1000),
+//             (Dir::S, Dir::W) => Some(1000),
+//             (Dir::W, Dir::N) => Some(1000),
+//             (Dir::W, Dir::S) => Some(1000),
+//             _ => None,
+//         }
+//     }
+//     // Traavelling
+//     else if (n1.i == n2.i && n1.j + 1 == n2.j && n1.dir == n2.dir && n1.dir == Dir::E)
+//         || (n1.i == n2.i && n1.j == n2.j + 1 && n1.dir == n2.dir && n1.dir == Dir::W)
+//         || (n1.i + 1 == n2.i && n1.j == n2.j && n1.dir == n2.dir && n1.dir == Dir::S)
+//         || (n1.i == n2.i + 1 && n1.j == n2.j && n1.dir == n2.dir && n1.dir == Dir::N)
+//     {
+//         Some(1)
+//     } else {
+//         None
+//     }
+// }
+
+// // Same but about 3x faster
+// fn get_edge_weight2(n1: &Node, n2: &Node) -> Option<u32> {
+//     if n1.i == n2.i && n1.j + 1 == n2.j {
+//         match (n1.dir, n2.dir) {
+//             (Dir::E, Dir::E) => Some(1),
+//             (Dir::N, Dir::E) => Some(1001),
+//             (Dir::S, Dir::E) => Some(1001),
+//             _ => None,
+//         }
+//     } else if n1.i == n2.i && n1.j == n2.j + 1 {
+//         match (n1.dir, n2.dir) {
+//             (Dir::W, Dir::W) => Some(1),
+//             (Dir::N, Dir::W) => Some(1001),
+//             (Dir::S, Dir::W) => Some(1001),
+//             _ => None,
+//         }
+//     } else if n1.i + 1 == n2.i && n1.j == n2.j {
+//         match (n1.dir, n2.dir) {
+//             (Dir::S, Dir::S) => Some(1),
+//             (Dir::E, Dir::S) => Some(1001),
+//             (Dir::W, Dir::S) => Some(1001),
+//             _ => None,
+//         }
+//     } else if n1.i == n2.i + 1 && n1.j == n2.j {
+//         match (n1.dir, n2.dir) {
+//             (Dir::N, Dir::N) => Some(1),
+//             (Dir::E, Dir::N) => Some(1001),
+//             (Dir::W, Dir::N) => Some(1001),
+//             _ => None,
+//         }
+//     } else {
+//         None
+//     }
+// }
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+struct Pos {
     i: usize,
     j: usize,
+}
+impl Pos {
+    fn new(i: usize, j: usize) -> Self {
+        Pos { i, j }
+    }
+    fn next(&self, d: Dir) -> Self {
+        match d {
+            Dir::N => Pos::new(self.i - 1, self.j),
+            Dir::S => Pos::new(self.i + 1, self.j),
+            Dir::E => Pos::new(self.i, self.j + 1),
+            Dir::W => Pos::new(self.i, self.j - 1),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+struct Node {
+    pos: Pos,
     dir: Dir,
 }
-
-// Get the cost of travelling from n1 to n2
-fn _get_edge_weight(n1: &Node, n2: &Node) -> Option<u32> {
-    // Rotating
-    if n1.i == n2.i && n1.j == n2.j {
-        match (n1.dir, n2.dir) {
-            (Dir::N, Dir::E) => Some(1000),
-            (Dir::N, Dir::W) => Some(1000),
-            (Dir::E, Dir::N) => Some(1000),
-            (Dir::E, Dir::S) => Some(1000),
-            (Dir::S, Dir::E) => Some(1000),
-            (Dir::S, Dir::W) => Some(1000),
-            (Dir::W, Dir::N) => Some(1000),
-            (Dir::W, Dir::S) => Some(1000),
-            _ => None,
+impl Node {
+    fn new(i: usize, j: usize, dir: Dir) -> Self {
+        Node {
+            pos: Pos::new(i, j),
+            dir,
         }
     }
-    // Traavelling
-    else if (n1.i == n2.i && n1.j + 1 == n2.j && n1.dir == n2.dir && n1.dir == Dir::E)
-        || (n1.i == n2.i && n1.j == n2.j + 1 && n1.dir == n2.dir && n1.dir == Dir::W)
-        || (n1.i + 1 == n2.i && n1.j == n2.j && n1.dir == n2.dir && n1.dir == Dir::S)
-        || (n1.i == n2.i + 1 && n1.j == n2.j && n1.dir == n2.dir && n1.dir == Dir::N)
-    {
-        Some(1)
-    } else {
-        None
+    fn new_pos(pos: Pos, dir: Dir) -> Self {
+        Node { pos, dir }
     }
-}
-
-// Same but about 3x faster
-fn get_edge_weight2(n1: &Node, n2: &Node) -> Option<u32> {
-    if n1.i == n2.i && n1.j + 1 == n2.j {
-        match (n1.dir, n2.dir) {
-            (Dir::E, Dir::E) => Some(1),
-            (Dir::N, Dir::E) => Some(1001),
-            (Dir::S, Dir::E) => Some(1001),
-            _ => None,
+    fn edges(&self, grid: &Grid) -> Vec<(Node, u32)> {
+        let mut edges = Vec::new();
+        let next_pos = self.pos.next(self.dir);
+        if grid.pos.contains(&next_pos) {
+            edges.push((Node::new_pos(next_pos, self.dir), 1));
         }
-    } else if n1.i == n2.i && n1.j == n2.j + 1 {
-        match (n1.dir, n2.dir) {
-            (Dir::W, Dir::W) => Some(1),
-            (Dir::N, Dir::W) => Some(1001),
-            (Dir::S, Dir::W) => Some(1001),
-            _ => None,
+        let next_pos = self.pos.next(self.dir.clockwise());
+        if grid.pos.contains(&next_pos) {
+            edges.push((Node::new_pos(next_pos, self.dir.clockwise()), 1001));
         }
-    } else if n1.i + 1 == n2.i && n1.j == n2.j {
-        match (n1.dir, n2.dir) {
-            (Dir::S, Dir::S) => Some(1),
-            (Dir::E, Dir::S) => Some(1001),
-            (Dir::W, Dir::S) => Some(1001),
-            _ => None,
+        let next_pos = self.pos.next(self.dir.anticlockwise());
+        if grid.pos.contains(&next_pos) {
+            edges.push((Node::new_pos(next_pos, self.dir.anticlockwise()), 1001));
         }
-    } else if n1.i == n2.i + 1 && n1.j == n2.j {
-        match (n1.dir, n2.dir) {
-            (Dir::N, Dir::N) => Some(1),
-            (Dir::E, Dir::N) => Some(1001),
-            (Dir::W, Dir::N) => Some(1001),
-            _ => None,
-        }
-    } else {
-        None
+        edges
     }
 }
 
 #[derive(Debug)]
 struct Grid {
-    nodes: Vec<Node>,
-    start_node: Node,
-    end: (usize, usize),
+    pos: Vec<Pos>,
+    start_pos: Pos,
+    end_pos: Pos,
 }
 
+type NodeIndex = usize;
+
 struct Graph {
-    edges: Vec<(usize, usize, u32)>,
-    start_idx: usize,
-    end_idxs: Vec<usize>,
+    edges: Vec<(NodeIndex, NodeIndex, u32)>,
+    start_idx: NodeIndex,
+    end_idxs: Vec<NodeIndex>,
     n_nodes: usize,
 }
 
 impl Grid {
-    // Returns a graph containing:
-    //   - The edges of the graph: each edge is itself a 3-tuple of (node1, node2, weight)
-    //   - The starting node
-    //   - The possible ending nodes
-    //   - The number of nodes in the graph
-    // Note that nodes are represented by indices rather than their positions, because
-    // after this function there is no need to know their positions any more
-    fn to_graph(&self) -> Graph {
-        let mut edges = Vec::new();
+    // Get all possible nodes
+    fn all_nodes_and_idxs(&self) -> HashMap<Node, NodeIndex> {
+        let mut m = HashMap::new();
         let mut counter = 0;
-        let mut nodes_lookup = HashMap::new();
+        self.pos
+            .iter()
+            .flat_map(|pos| {
+                vec![
+                    Node::new_pos(*pos, Dir::N),
+                    Node::new_pos(*pos, Dir::S),
+                    Node::new_pos(*pos, Dir::E),
+                    Node::new_pos(*pos, Dir::W),
+                ]
+            })
+            .for_each(|node| {
+                m.insert(node, counter);
+                counter += 1;
+            });
+        m
+    }
+
+    fn to_graph(&self) -> Graph {
+        let all_nodes_and_idxs = self.all_nodes_and_idxs();
+        let mut all_edges = Vec::new();
         let mut start_idx = None;
         let mut end_idxs = Vec::new();
-        for (idx1, node_i) in self.nodes.iter().enumerate() {
-            if node_i == &self.start_node {
-                start_idx = Some(idx1);
+
+        for (node, idx) in all_nodes_and_idxs.iter() {
+            if node.pos == self.start_pos && node.dir == Dir::E {
+                start_idx = Some(idx);
             }
-            if (node_i.i, node_i.j) == self.end {
-                end_idxs.push(idx1);
+            if node.pos == self.end_pos {
+                end_idxs.push(*idx);
             }
-            for (idx2, node_j) in self.nodes.iter().enumerate() {
-                if let Some(weight) = get_edge_weight2(node_i, node_j) {
-                    edges.push((idx1, idx2, weight));
-                    if let std::collections::hash_map::Entry::Vacant(e) = nodes_lookup.entry(idx1) {
-                        e.insert(counter);
-                        counter += 1;
+            // Collect edges
+            for (next_node, weight) in node.edges(self) {
+                match all_nodes_and_idxs.get(&next_node) {
+                    Some(next_idx) => {
+                        all_edges.push((*idx, *next_idx, weight));
                     }
-                    if let std::collections::hash_map::Entry::Vacant(e) = nodes_lookup.entry(idx2) {
-                        e.insert(counter);
-                        counter += 1;
-                    }
+                    None => panic!("Shouldn't happen"),
                 }
             }
         }
-        // Reindex to strip out all nodes that can't be reached
-        let edges = edges
-            .into_iter()
-            .map(|(n1, n2, w)| (nodes_lookup[&n1], nodes_lookup[&n2], w))
-            .collect();
-        let start_idx = nodes_lookup[&start_idx.expect("Start node not found")];
-        let end_idxs = end_idxs.into_iter().map(|idx| nodes_lookup[&idx]).collect();
+
         Graph {
-            edges,
-            start_idx,
+            edges: all_edges,
+            start_idx: *start_idx.expect("No start node found"),
             end_idxs,
-            n_nodes: nodes_lookup.len(),
+            n_nodes: all_nodes_and_idxs.len(),
         }
     }
 
@@ -165,26 +234,18 @@ impl Grid {
                     acc
                 }
             });
+            // Remove that node
+            unvisited.remove(&n);
+            // If the remaining nodes are unreachable, break
             if distances[n] == u32::MAX {
                 break;
             }
             // Extract the edges that touch the node of interest
-            let edges = graph
-                .edges
-                .clone()
-                .into_iter()
-                .filter(|e| e.0 == n)
-                .collect::<Vec<_>>();
-            // Fill in any weights
-            edges.into_iter().for_each(|(_, n2, weight)| {
-                distances[n2] = distances[n2].min(distances[n] + weight);
+            graph.edges.iter().for_each(|(n1, n2, weight)| {
+                if *n1 == n {
+                    distances[*n2] = distances[*n2].min(distances[n] + weight);
+                }
             });
-            // Remove that node
-            unvisited.remove(&n);
-            // If we've already visited all the target nodes, break
-            if graph.end_idxs.iter().all(|idx| !unvisited.contains(idx)) {
-                break;
-            }
         }
 
         // Then pick the end node with the lowest weight
@@ -202,52 +263,26 @@ impl From<&str> for Grid {
         let mut end = None;
         for (i, line) in input.lines().enumerate() {
             for (j, char) in line.chars().enumerate() {
+                let pos = Pos::new(i, j);
                 match char {
                     '#' => continue,
-                    '.' => legal_points.push((i, j)),
+                    '.' => legal_points.push(pos),
                     'E' => {
-                        legal_points.push((i, j));
-                        end = Some((i, j));
+                        legal_points.push(pos);
+                        end = Some(pos);
                     }
                     'S' => {
-                        legal_points.push((i, j));
-                        start = Some(Node { i, j, dir: Dir::E });
+                        legal_points.push(pos);
+                        start = Some(pos);
                     }
                     _ => panic!("Invalid character in input"),
                 }
             }
         }
-        let nodes = legal_points
-            .iter()
-            .flat_map(|(i, j)| {
-                vec![
-                    Node {
-                        i: *i,
-                        j: *j,
-                        dir: Dir::N,
-                    },
-                    Node {
-                        i: *i,
-                        j: *j,
-                        dir: Dir::S,
-                    },
-                    Node {
-                        i: *i,
-                        j: *j,
-                        dir: Dir::E,
-                    },
-                    Node {
-                        i: *i,
-                        j: *j,
-                        dir: Dir::W,
-                    },
-                ]
-            })
-            .collect();
         Grid {
-            nodes,
-            start_node: start.expect("No start point found"),
-            end: end.expect("No end point found"),
+            pos: legal_points,
+            start_pos: start.expect("No start point found"),
+            end_pos: end.expect("No end point found"),
         }
     }
 }
