c477a9fc7855dca9647b3e5a68918878df73914c
Penelope Yong <penelopeysm@gmail.com>
2024-12-20 14:54:35 +0000

    Even shorter

--- src/bin/19golf.py
+++ src/bin/19golf.py
@@ -1,5 +1,5 @@
 n,_,*m,_=open("../../data/inputs/19.txt").read().split("\n")
 from functools import*
-f=cache(lambda x:sum([f(x[len(y):])for y in n.split(", ")if y<=x<y+'~'])if x else 1)
+f=cache(lambda x:sum([f(x[len(y):])for y in n.split(", ")if y<=x<y+"~"])or""==x)
 *g,=map(f,m)
 print(sum(map(bool,g)),sum(g))
